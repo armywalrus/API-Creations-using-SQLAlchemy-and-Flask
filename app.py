a3ae6373078c007d6dbceeae6824414eac6f7334
@armywalrus <armywalrus@gmail.com>
2020-08-18 17:47:27 -0400

    added dict for precip api; started query for start api

--- app.py
+++ app.py
@@ -30,43 +30,77 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+# Create a database session object
 session = Session(engine)
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
+        f"Available Routes:<br/><br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precip():
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    last_year_precip = session.query(Measurements.date, Measurements.prcp).filter(Measurements.date >= year_ago).all()  
+    last_year_precip = session.query(Measurements.date, Measurements.prcp).filter(Measurements.date >= year_ago).all() 
+    session.close() 
     return jsonify(last_year_precip)
 
 @app.route("/api/v1.0/stations")
 def stations():
     active_station = session.query(Measurements.station, func.count(Measurements.station)).group_by(Measurements.station).\
     order_by(func.count(Measurements.station).desc()).all()
+    all_stations = []
+    for station, count in results:
+        all_stations = {}
+        all_stations["station"] = station
+        all_stations["count"] = count
+        all_stations.append(all_stations)
+    session.close() 
     return jsonify(active_station)
     
 @app.route("/api/v1.0/tobs")
 def temps():
     lowest_temp = session.query(Measurements.station, func.min(Measurements.tobs), func.max(Measurements.tobs), func.avg(Measurements.tobs)).\
                             filter(Measurements.station == "USC00519281").all()
+    session.close() 
     return jsonify(lowest_temp)
 
-# @app.route("/api/v1.0/<start>")
-# def start():
+@app.route("/api/v1.0/start")
+def start():
+    #Design a query that lists the min/max/avg totals for each station 
+    sel = [Measurements.station, 
+       func.min(Measurements.tobs), 
+       func.max(Measurements.tobs), 
+       func.avg(Measurements.tobs)]
+       
+    start = session.query(*sel).\
+        group_by(Measurements.station).\
+        order_by(Measurements.station).all()
+    session.close()  
+    return jsonify(start) 
 
-
-# @app.route("/api/v1.0/<start>/<end>")
+# @app.route("/api/v1.0/start/end")
 # def end():
+    #Design a query that lists the min/max/avg totals for each station 
+    # sel = [Measurements.station, 
+    #    func.min(Measurements.tobs), 
+    #    func.max(Measurements.tobs), 
+    #    func.avg(Measurements.tobs)]
+       
+    # start = session.query(*sel).\
+    #     group_by(Measurements.station).\
+    #     order_by(Measurements.station).all()
+    # session.close()  
+    # return jsonify(start) 
+
 
 
 
